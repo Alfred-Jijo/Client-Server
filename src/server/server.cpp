0e17d94b7911d44894764955bfcc9b99cf613e8d
Alfred-Jijo <alfredjijo28@gmail.com>
2024-04-04 21:41:56 +0100

    data and socket

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,1 +1,26 @@
-int main() { return 0; }
+#include <iostream>
+#include <string>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0)
+    {
+        std::cerr << "WSAStartup failed: " << iResult << std::endl;
+        return 1;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Error creating server socket: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+        return 0;
+}
