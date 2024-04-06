fc0b8c85e556c18139f533651b9792a773a5a88e
Alfred-Jijo <alfredjijo28@gmail.com>
2024-04-06 15:59:21 +0100

    formatting

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -4,88 +4,82 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-void executeCommand(const std::string &command)
-{
-    size_t startPos = command.find("--");
-    size_t endPos = command.rfind("--");
+void executeCommand(const std::string &command) {
+  size_t startPos = command.find("--");
+  size_t endPos = command.rfind("--");
 
-    if (startPos != std::string::npos && endPos != std::string::npos && endPos > startPos + 2)
-    {
-        std::string commandString = command.substr(startPos + 2, endPos - startPos - 2);
-        system(commandString.c_str());
-    }
+  if (startPos != std::string::npos && endPos != std::string::npos &&
+      endPos > startPos + 2) {
+    std::string commandString =
+        command.substr(startPos + 2, endPos - startPos - 2);
+    system(commandString.c_str());
+  }
 }
 
-int main()
-{
-    WSADATA wsaData;
-    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0)
-    {
-        std::cerr << "WSAStartup failed: " << iResult << std::endl;
-        return 1;
-    }
+int main() {
+  WSADATA wsaData;
+  int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (iResult != 0) {
+    std::cerr << "WSAStartup failed: " << iResult << std::endl;
+    return 1;
+  }
 
-    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (serverSocket == INVALID_SOCKET)
-    {
-        std::cerr << "Error creating server socket: " << WSAGetLastError() << std::endl;
-        WSACleanup();
-        return 1;
-    }
+  SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (serverSocket == INVALID_SOCKET) {
+    std::cerr << "Error creating server socket: " << WSAGetLastError()
+              << std::endl;
+    WSACleanup();
+    return 1;
+  }
 
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+  sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    std::cout << "Enter the port to listen on: ";
-    unsigned short port;
-    std::cin >> port;
-    serverAddr.sin_port = htons(port);
+  std::cout << "Enter the port to listen on: ";
+  unsigned short port;
+  std::cin >> port;
+  serverAddr.sin_port = htons(port);
 
-    if (bind(serverSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-    {
-        std::cerr << "Bind failed: " << WSAGetLastError() << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR)
-    {
-        std::cerr << "Listen failed: " << WSAGetLastError() << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    std::cout << "Server is listening on port " << port << std::endl;
-
-    SOCKET clientSocket = accept(serverSocket, NULL, NULL);
-    if (clientSocket == INVALID_SOCKET)
-    {
-        std::cerr << "Accept failed: " << WSAGetLastError() << std::endl;
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    char buffer[1024];
-    int bytesReceived;
-    do
-    {
-        bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytesReceived > 0)
-        {
-            std::string message(buffer, bytesReceived);
-            std::cout << "Received message: " << message << std::endl;
-            executeCommand(message);
-        }
-    } while (bytesReceived > 0);
-
-    closesocket(clientSocket);
+  if (bind(serverSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) ==
+      SOCKET_ERROR) {
+    std::cerr << "Bind failed: " << WSAGetLastError() << std::endl;
     closesocket(serverSocket);
     WSACleanup();
+    return 1;
+  }
 
-    return 0;
+  if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+    std::cerr << "Listen failed: " << WSAGetLastError() << std::endl;
+    closesocket(serverSocket);
+    WSACleanup();
+    return 1;
+  }
+
+  std::cout << "Server is listening on port " << port << std::endl;
+
+  SOCKET clientSocket = accept(serverSocket, NULL, NULL);
+  if (clientSocket == INVALID_SOCKET) {
+    std::cerr << "Accept failed: " << WSAGetLastError() << std::endl;
+    closesocket(serverSocket);
+    WSACleanup();
+    return 1;
+  }
+
+  char buffer[1024];
+  int bytesReceived;
+  do {
+    bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesReceived > 0) {
+      std::string message(buffer, bytesReceived);
+      std::cout << "Received message: " << message << std::endl;
+      executeCommand(message);
+    }
+  } while (bytesReceived > 0);
+
+  closesocket(clientSocket);
+  closesocket(serverSocket);
+  WSACleanup();
+
+  return 0;
 }
